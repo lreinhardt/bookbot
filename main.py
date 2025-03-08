75d8dbb27cb63435bb18203dc7f04b4d6d57ae69
user <user@example.com>
2025-03-08 18:23:58 +0100

    finished stats.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from stats import count_words
+import sys
+
+from stats import count_words, count_characters, sort_characters_count
 
 def get_book_text(fp):
     with open(fp) as f:
         return f.read()
 
 def main():
-    booktext = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    fp = sys.argv[1]
+    booktext = get_book_text(fp)
     num_words = count_words(booktext)
-    print(f"{num_words} words found in the document")
+
+    char_count = sort_characters_count(count_characters(booktext))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {fp}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in char_count:
+        print(f"{i['character']}: {i['count']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,30 @@
 def count_words(s):
     return len(s.split())
+
+
+def count_characters(s:str):
+    d = {}
+
+    for c in s.lower():
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    
+    return d
+
+
+def sort_on(d:dict):
+    return d["count"]
+
+
+def sort_characters_count(d:dict[str,int]):
+    sorted_list = []
+
+    for c in d:
+        if not c.isalpha():
+            continue
+        sorted_list.append({"character": c, "count": d[c]})
+        sorted_list.sort(reverse=True, key=sort_on)
+    
+    return sorted_list
