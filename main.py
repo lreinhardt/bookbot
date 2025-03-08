0d4eb34c135f65965c2de1cd81ef6895d1d9835f
user <user@example.com>
2025-03-08 17:47:10 +0100

    refactoring count_words into stats.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+from stats import count_words
+
+def get_book_text(fp):
+    with open(fp) as f:
+        return f.read()
+
+def main():
+    booktext = get_book_text("books/frankenstein.txt")
+    num_words = count_words(booktext)
+    print(f"{num_words} words found in the document")
+
+main()
